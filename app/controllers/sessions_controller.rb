d3896dd458b9fa6953da553eaeb8df814654da54
Daniel Lee <daniellee0228@berkeley.edu>
2019-04-03 19:24:11 +0000

    Added Navbar and little bit more signup/login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
+  
   def new
   end
   
   def create
-
     user = User.find_by(email: params[:session][:email].downcase)
     
     if user && user.authenticate(params[:session][:password])
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have logged out."
+    flash[:success] = "You have successfully logged out."
     redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         
         if @user.save
+            session[:user_id] = @user.id
             flash[:success] = "Welcome to MultiMeet #{@user.username}!"
             redirect_to projects_path
         else
