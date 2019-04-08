870d700e145b36d508ae4a1f17827bef2bebed40
Lawrence Chen <lawrencechen14@gmail.com>
2019-04-07 23:46:14 -0700

    progress towards project creation feature, does not save to db yet

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,25 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.update(project_name: :project_name)
+    @project.update(username: :username)
+
+    # print(@project)
+
+    if @project.save
+      flash[:success] = "Successfully created #{@project.project_name}!"
+      redirect_to projects_path
+    else
+      flash[:message] = @user.errors.full_messages
+      redirect_to projects_path
+    end
+  end
+
   def index
     @user = current_user
     print(@user)
@@ -6,5 +27,10 @@ class ProjectsController < ApplicationController
       print(@user)
       @projects = Project.where(username: @user.username)
     end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:project_name, :username)
   end
 end
