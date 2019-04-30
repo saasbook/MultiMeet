4aeaecfc7a6e4f3dc95e7ff8100257e0e7b735ba
Turese Anderson <turese@berkeley.edu>
2019-04-29 22:31:21 -0700

    possibly buggy function for checking boxes

--- features/step_definitions/enter_preferences_steps.rb
+++ features/step_definitions/enter_preferences_steps.rb
@@ -17,3 +17,15 @@ When /^I access the time ranking page for project "(.*)" from email "(.*)" and s
 	
 	visit "/projects/" + String(project_id) + "/participants/" + String(participant_id) + "/ranking&secretid=" + secretid 
 end
+
+When /^I choose "(.*)" for time "(.*)"$/ do |option, time|
+  pending # Write code here that turns the phrase above into concrete actions
+  time = ProjectTime.find_by(:date_time => Time.parse(row[:datetime]))
+  if option.eql?("Preferred")
+    choose(time.id, option: 1) # name and value to make unique
+  elsif option.eql?("Okay")
+    choose(time.id, option: 2) # name and value to make unique
+  else
+    choose(time.id, option: 3) # name and value to make unique
+  end
+end
