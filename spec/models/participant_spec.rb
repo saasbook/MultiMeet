f420dced5ae53b304fe1d359c45e165ce35a8e59
Aakup <jinhuili12121212@gmail.com>
2019-04-29 19:53:40 -0700

    styles updates

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
-  it "allows us to create a participant under project" do
+  it 'allows us to create a participant under project' do
     # create a project
     project = create(:project)
 
