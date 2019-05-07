49c5495ba0aec7e6fdb54c821ec48b8deeda152a
Joyce Luong <hello@jsluong.com>
2019-05-07 14:46:15 -0700

    config boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,4 +2,7 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+ENV['MAILER_EMAIL']='multimeetemailer@gmail.com'
+ENV['MAILER_PASSWORD']='ilove169!'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
