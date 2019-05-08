f0bd4f1e0f623cfee17234032d10f4557b3d2da8
Lawrence Chen <lawrencechen14@gmail.com>
2019-05-07 18:25:59 -0700

    Fixing renaming issues and other minor routing tweaks

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -23,26 +23,21 @@ class ProjectsController < ApplicationController
   # end
 
   def update
-    # byebug
     @project = Project.find(params[:id].to_i)
     new_project_name = project_params[:project_name]
 
-    if valid_project_name? new_project_name
+    if valid_renaming? new_project_name
       @project.update(project_name: new_project_name)
       flash[:success] = "Successfully renamed project to #{new_project_name}"
-      redirect_to edit_project_path(@project)
+      redirect_to project_path(@project)
     else
       redirect_to edit_project_path(@project)
     end
-
   end
 
   # returns true if ok, false if not
   def valid_project_name?(project_name)
-    if @project.project_name == project_name
-      flash[:danger] = 'Cannot rename to same project name'
-      return false
-    elsif project_name.nil? || project_name.empty?
+    if project_name.nil? || project_name.empty?
       flash[:danger] = 'Invalid project name'
       return false
     elsif !Project.where(
@@ -52,6 +47,14 @@ class ProjectsController < ApplicationController
       return false
     end
     true
+  end
+
+  def valid_renaming?(project_name)
+    if @project.project_name == project_name
+      flash[:danger] = 'Cannot rename to same project name'
+      return false
+    end
+    valid_project_name? project_name
   end
 
   def redirect_based_on_create_type(project_name)
